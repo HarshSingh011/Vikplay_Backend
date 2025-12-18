6da444ab08aa763673e69c65591c73ccee31ed8a
HarshSingh011 <harsh2004416@gmail.com>
2025-12-18 14:38:20 +0530

    auth caching

--- auth/routes/auth.py
+++ auth/routes/auth.py
@@ -291,7 +291,7 @@ async def login_user(user_data: UserLogin, db: Session = Depends(get_db)):
         return TokenResponse(
             access_token=access_token,
             token_type="bearer",
-            user=UserResponse.from_orm(user)
+            user=UserResponse.model_validate(user)
         )
         
     except HTTPException:
